ccf4125169bc377bc423706d505c16028a29492c
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-02-19 15:47:56 +0300

    feat(mini-react-webpack): use functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./app.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Hey, welcome to mini react webpack!</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hey, welcome to mini react webpack!</h1>
+    </div>
+  );
+};
 
 export default App;
