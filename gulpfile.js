77cb720b6ef82376470268798c1481ff0980d22b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-26 01:35:06 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 const gulp = require('gulp');
 const { join } = require('path');
 
-gulp.task('copy', function () {
-	return gulp
-		.src(['safelink.html', 'moment-timezone.html', 'index.html', 'bot-detect.{html,js}', 'cookies.html', 'disqus-comment.html'], {
+gulp.task('copy', function (done) {
+	const finish = () => done();
+	gulp
+		.src(
+			[
+				'safelink.html',
+				'moment-timezone.html',
+				'index.html',
+				'bot-detect.{html,js}',
+				'cookies.html',
+				'disqus-comment.html',
+				'package.json',
+			],
+			{
+				cwd: __dirname,
+			},
+		)
+		.pipe(gulp.dest(join(__dirname, 'build')));
+	gulp
+		.src(['assets/**/*'], {
 			cwd: __dirname,
 		})
-		.pipe(gulp.dest(join(__dirname, 'build')));
+		.pipe(gulp.dest(join(__dirname, 'build/assets')))
+		.once('end', finish);
 });
