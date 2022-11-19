cc40da51a8f31617bef408a7eb8c67d3718db3f3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-11-19 15:53:33 +0700

    update moment timezone

--- moment-timezone.js
+++ moment-timezone.js
@@ -1,81 +1,84 @@
+/* eslint-disable no-undef */
 // https://codepen.io/dimaslanjaka/pen/LYegjaV
 
-if (location.host.includes("cdpn")) console.clear();
-const idate = document.querySelector("input#date");
-const pattern = document.querySelector("input#pattern");
-const result = document.querySelector("#moment-result");
+if (location.host.includes('cdpn')) console.clear();
+const idate = document.querySelector('input#date');
+const pattern = document.querySelector('input#pattern');
+const result = document.querySelector('#moment-result');
 
 function moment_format(datestr) {
-  return moment(datestr)
-    .tz("Asia/Jakarta")
-    .format(pattern.value || "");
+	/*moment.tz.add([
+		'America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0',
+		'America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0',
+		'Asia/Jakarta',
+	]);*/
+	//moment.tz.link('America/Los_Angeles|US/Pacific|Asia/Jakarta');
+
+	return moment(datestr)
+		.tz('Asia/Jakarta')
+		.format(pattern.value || '');
 }
+
 function update() {
-  idate.value = new Date().toString();
-  const formatted = moment_format(idate.value);
-  result.textContent = formatted;
+	idate.value = new Date().getTime();
+	const formatted = moment_format(idate.value);
+	result.textContent = formatted;
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  update(); // update value on-load
-  let interval;
-  document
-    .getElementById("start-interval")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      if (!interval) interval = setInterval(update, 1000);
-    });
-  document
-    .getElementById("stop-interval")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      clearInterval(interval);
-      interval = null;
-    });
+document.addEventListener('DOMContentLoaded', function (_e) {
+	update(); // update value on-load
+	let interval;
+	document.getElementById('start-interval').addEventListener('click', function (e) {
+		e.preventDefault();
+		if (!interval) interval = setInterval(update, 1000);
+	});
+	document.getElementById('stop-interval').addEventListener('click', function (e) {
+		e.preventDefault();
+		clearInterval(interval);
+		interval = null;
+	});
 
-  document
-    .getElementById("copy-moment")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      copyTextToClipboard(result.textContent.trim());
-    });
-  pattern.addEventListener("change", update);
+	document.getElementById('copy-moment').addEventListener('click', function (e) {
+		e.preventDefault();
+		copyTextToClipboard(result.textContent.trim());
+	});
+	pattern.addEventListener('change', update);
 });
 
 function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
+	var textArea = document.createElement('textarea');
+	textArea.value = text;
 
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
+	// Avoid scrolling to bottom
+	textArea.style.top = '0';
+	textArea.style.left = '0';
+	textArea.style.position = 'fixed';
 
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
 
-  try {
-    var successful = document.execCommand("copy");
-    var msg = successful ? "successful" : "unsuccessful";
-    console.log("Fallback: Copying text command was " + msg);
-  } catch (err) {
-    console.error("Fallback: Oops, unable to copy", err);
-  }
+	try {
+		var successful = document.execCommand('copy');
+		var msg = successful ? 'successful' : 'unsuccessful';
+		console.log('Fallback: Copying text command was ' + msg);
+	} catch (err) {
+		console.error('Fallback: Oops, unable to copy', err);
+	}
 
-  document.body.removeChild(textArea);
+	document.body.removeChild(textArea);
 }
 function copyTextToClipboard(text) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
-  }
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log("Async: Copying to clipboard was successful!");
-    },
-    function (err) {
-      console.error("Async: Could not copy text: ", err);
-    }
-  );
+	if (!navigator.clipboard) {
+		fallbackCopyTextToClipboard(text);
+		return;
+	}
+	navigator.clipboard.writeText(text).then(
+		function () {
+			console.log('Async: Copying to clipboard was successful!');
+		},
+		function (err) {
+			console.error('Async: Could not copy text: ', err);
+		},
+	);
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
+app.use('node_modules', express.static(__dirname, 'node_modules'));
 
 app.listen(4000, function () {
 	console.log('http://localhost:4000');
