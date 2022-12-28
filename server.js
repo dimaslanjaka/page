e5baa926ba1c9f1a68551575952e3f89aaf69826
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-28 12:25:41 +0700

    add axios cache interceptor

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ require('./gulpfile');
 const inject = require('connect-browser-sync');
 const { join } = require('path');
 const compileNJK = GulpClient.series('compile');
-const axios = require('axios');
+const Axios = require('axios');
+const { setupCache } = require('axios-cache-interceptor');
+const axios = setupCache(Axios);
 
 compileNJK(function () {
 	const app = express();
@@ -31,16 +33,14 @@ compileNJK(function () {
 	app.use(express.static(__dirname));
 	app.use('/page', express.static(__dirname));
 	app.use('/node_modules', express.static(join(__dirname, 'node_modules')));
-	app.use('/favicon.ico', function (_, res) {
-		axios
-			.get('http://www.google.com/s2/favicons?domain=www.webmanajemen.com')
-			.then(response => {
-				console.log(response);
-				res.send(response);
-			})
-			.catch(err => {
-				console.log(err);
-			});
+	app.use('/favicon.ico', async function (_, res) {
+		try {
+			const response = await axios.default.get('http://www.google.com/s2/favicons?domain=www.webmanajemen.com');
+			res.set('Content-Type', response.headers['content-type']);
+			res.send(response.data);
+		} catch (err) {
+			console.log(err);
+		}
 	});
 
 	app.listen(4000, function () {
