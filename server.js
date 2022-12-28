3f9d98f689248ca7ca8866517d7399aa5d471f32
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-28 12:15:31 +0700

    add axios for serverside request

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('./gulpfile');
 const inject = require('connect-browser-sync');
 const { join } = require('path');
 const compileNJK = GulpClient.series('compile');
+const axios = require('axios');
 
 compileNJK(function () {
 	const app = express();
@@ -30,6 +31,17 @@ compileNJK(function () {
 	app.use(express.static(__dirname));
 	app.use('/page', express.static(__dirname));
 	app.use('/node_modules', express.static(join(__dirname, 'node_modules')));
+	app.use('/favicon.ico', function (_, res) {
+		axios
+			.get('http://www.google.com/s2/favicons?domain=www.webmanajemen.com')
+			.then(response => {
+				console.log(response);
+				res.send(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	});
 
 	app.listen(4000, function () {
 		console.log('http://localhost:4000');
