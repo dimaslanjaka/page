935a2f6de2acf199b4fe7d424a03f4c51edead9e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-10 16:42:30 +0700

    refactor: migrate to new builder

--- server.js
+++ server.js
@@ -1,46 +1,82 @@
 const express = require('express');
-const GulpClient = require('gulp');
 const browserSync = require('browser-sync');
-require('./gulpfile');
 const inject = require('connect-browser-sync');
-const { join } = require('path');
-const compileNJK = GulpClient.series('compile');
 const { mkdirpSync, existsSync, readFileSync } = require('fs-extra');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const sass = require('./src/node-sass-middleware');
 
-const tmpdir = join(__dirname, 'tmp');
-if (!existsSync(tmpdir)) mkdirpSync(tmpdir);
 
-compileNJK(function () {
-	const app = express();
-	const bs = browserSync.create().init({
-		logSnippet: false,
-		files: [
-			__dirname,
-			{
-				match: ['**/*.njk', '**/*.scss'],
-				fn: function (event, file) {
-					/** Custom event handler **/
-					console.log('[Browsersync]', event, file);
-					compileNJK(null);
-				},
+const app = express();
+
+// browser-sync start
+const bs = browserSync.create().init({
+	logSnippet: false,
+	files: [
+		__dirname,
+		{
+			match: ['**/*.njk', '**/*.scss'],
+			fn: function (event, file) {
+				/** Custom event handler **/
+				console.log('[Browsersync]', event, file);
 			},
-		],
-		ignore: ['**/.git*', '**/tmp/**', '**/build/**'],
-		cors: true,
-	});
+		},
+	],
+	ignore: ['**/.git*', '**/tmp/**', '**/build/**'],
+	cors: true,
+});
+app.use(inject(bs));
+// browser-sync ends
 
-	app.use(inject(bs));
+// engine start
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app,
+});
 
-	app.use(express.static(__dirname));
-	app.use('/page', express.static(__dirname));
-	app.use('/node_modules', express.static(join(__dirname, 'node_modules')));
-	app.use('/favicon.ico', async function (_, res) {
-		const read = readFileSync(join(__dirname, 'assets/img/w-icon-25.png'));
-		res.setHeader('content-type', 'image/png');
-		res.send(read);
-	});
+app.set('view engine', 'html');
 
-	app.listen(4000, function () {
-		console.log('http://localhost:4000');
+app.use(
+	sass({
+		src: __dirname + '/source', // Input SASS files
+		dest: __dirname + '/page', // Output CSS
+		debug: true,
+	}),
+);
+// engine ends
+
+// server static files
+app.use(express.static(path.join(__dirname, 'source')));
+app.use('/page', express.static(path.join(__dirname, 'source')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/favicon.ico', async function (_, res) {
+	const read = readFileSync(path.join(__dirname, 'source/assets/img/w-icon-25.png'));
+	res.setHeader('content-type', 'image/png');
+	res.send(read);
+});
+// static files ends
+
+// dynamic routes
+app.use('/page/:permalink', function (req, res) {
+	const { permalink } = req.params;
+	const basename = path.basename(permalink, path.extname(permalink));
+	const dirname = path.dirname(permalink);
+	const realpath = path.join(__dirname, 'views', dirname, basename + '.njk');
+	console.log({ dirname, basename, realpath });
+	res.render(realpath, {}, function (err, html) {
+		if (err) {
+			console.log('fail render', permalink);
+			res.render('404.njk');
+		} else {
+			console.log('success render', permalink);
+			res.send(html);
+		}
 	});
+});
+//
+
+// start server
+app.listen(4000, function () {
+	console.log('http://localhost:4000');
 });
