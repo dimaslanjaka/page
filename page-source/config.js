d80f99f8f22d3a702b6f32268ff135a54ade843c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-08 13:18:03 +0700

    feat: add bot detector

--- page-source/config.js
+++ page-source/config.js
@@ -15,5 +15,9 @@ module.exports = {
 			src: 'http://localhost:4000/page/cookies.html',
 			dest: 'page/cookies.html',
 		},
+		{
+			src: 'http://localhost:4000/page/bot-detect.html',
+			dest: 'page/bot-detect.html',
+		},
 	],
 };
