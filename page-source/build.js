1734b71e983c924cba3984657cb77a32d09cd5df
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-08 11:24:33 +0700

    chore: write into direct path from cwd

--- page-source/build.js
+++ page-source/build.js
@@ -4,14 +4,13 @@ const { writefile } = require('sbg-utility');
 const terserHtml = require('html-minifier-terser');
 const { default: _git } = require('git-command-helper');
 
-const routes = config.routes;
-
 async function build() {
+	const routes = config.routes;
 	for (let i = 0; i < routes.length; i++) {
 		const route = routes[i];
 		const response = await axios.get(route.src);
 		const minified = await terserHtml.minify(response.data, { collapseWhitespace: true });
-		writefile(route.dest.replace('__dirname', __dirname), minified);
+		writefile(route.dest, minified);
 	}
 	// _gulp.series('page:commit')();
 }
--- page-source/config.js
+++ page-source/config.js
@@ -5,15 +5,15 @@ module.exports = {
 	routes: [
 		{
 			src: 'http://localhost:4000/page/moment-timezone.html',
-			dest: '__dirname/../page/moment-timezone.html',
+			dest: 'page/moment-timezone.html',
 		},
 		{
 			src: 'http://localhost:4000/page/safelink.html',
-			dest: '__dirname/../page/safelink.html',
+			dest: 'page/safelink.html',
 		},
 		{
 			src: 'http://localhost:4000/page/cookies.html',
-			dest: '__dirname/../page/cookies.html',
+			dest: 'page/cookies.html',
 		},
 	],
 };
