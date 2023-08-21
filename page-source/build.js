76296f9520861bc2e69a6602eed62fb1a7e464a3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-21 21:25:23 +0700

    chore: remove browsersync script

--- page-source/build.js
+++ page-source/build.js
@@ -3,13 +3,19 @@ const config = require('./config');
 const { writefile, path } = require('sbg-utility');
 const terserHtml = require('html-minifier-terser');
 const { default: _git } = require('git-command-helper');
+const jsdom = require('jsdom').JSDOM;
 
 async function build() {
 	const routes = config.routes;
 	for (let i = 0; i < routes.length; i++) {
 		const route = routes[i];
 		const response = await axios.get(route.src);
-		const minified = await terserHtml.minify(response.data, { collapseWhitespace: true });
+		const dom = new jsdom(response.data);
+		const window = dom.window;
+		// remove browsersync script
+		window.document.querySelector('script#__bs_script__').remove();
+		const html = dom.serialize();
+		const minified = await terserHtml.minify(html, { collapseWhitespace: true });
 		writefile(path.join(process.cwd(), route.dest), minified);
 	}
 	// _gulp.series('page:commit')();
