9351080aa3183d98f9f9c2a5ef66d68782a58b6c
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-08-26 19:22:59 +0700

    minify on production

--- page-source/server-core.js
+++ page-source/server-core.js
@@ -1,25 +1,26 @@
-require('ts-node').register({ projectSearchDir: __dirname });
+require("ts-node").register({ projectSearchDir: __dirname });
 
-const { encodeURL } = require('hexo-util');
-const express = require('express');
-const fs = require('fs-extra');
-const nunjucks = require('nunjucks');
-const path = require('path');
-const { writefile } = require('sbg-utility');
-const { default: logger } = require('./src/logger');
-const sass = require('./src/node-sass-middleware2').default;
-const rollup = require('./src/rollup-middleware2').default;
-const browserSync = require('browser-sync');
-const inject = require('connect-browser-sync');
-const { default: git } = require('git-command-helper');
-const { isDev } = require('./src/utils');
-const { default: minifyHtmlMiddleware } = require('./src/html-render-minifier');
+const { encodeURL } = require("hexo-util");
+const express = require("express");
+const fs = require("fs-extra");
+const nunjucks = require("nunjucks");
+const path = require("path");
+const { writefile } = require("sbg-utility");
+const { default: logger } = require("./src/logger");
+const sass = require("./src/node-sass-middleware2").default;
+const rollup = require("./src/rollup-middleware2").default;
+const browserSync = require("browser-sync");
+const inject = require("connect-browser-sync");
+const { default: git } = require("git-command-helper");
+const { isDev } = require("./src/utils");
+const { default: minifyHtmlMiddleware } = require("./src/html-render-minifier");
+import terser from "@rollup/plugin-terser";
 
-const console = new logger('server');
+const console = new logger("server");
 const app = express();
 
 // engine start
-const view_path = path.join(__dirname, 'views');
+const view_path = path.join(__dirname, "views");
 
 /*const env = new nunjucks.Environment([new nunjucks.FileSystemLoader(view_path)], {
 	autoescape: true,
@@ -30,25 +31,25 @@ const env = nunjucks.configure(view_path, {
 	express: app,
 	noCache: isDev(),
 });
-env.addFilter('uriencode', str => {
+env.addFilter("uriencode", (str) => {
 	return encodeURL(str);
 });
-env.addFilter('noControlChars', str => {
-	return str.replace(/[\x00-\x1F\x7F]/g, ''); // eslint-disable-line no-control-regex
+env.addFilter("noControlChars", (str) => {
+	return str.replace(/[\x00-\x1F\x7F]/g, ""); // eslint-disable-line no-control-regex
 });
 // Extract date from datetime
 env.addFilter(
-	'formatDate',
+	"formatDate",
 	/**
 	 *
 	 * @param {import('moment-timezone').Moment} input
 	 * @returns
 	 */
-	input => {
+	(input) => {
 		return input.toISOString().substring(0, 10);
-	},
+	}
 );
-env.addGlobal('css', str => {
+env.addGlobal("css", (str) => {
 	return `
 <link
 	rel="preload"
@@ -62,36 +63,37 @@ env.addGlobal('css', str => {
 	`.trim();
 });
 
-app.engine('html', env.render);
-app.set('view engine', 'html');
+app.engine("html", env.render);
+app.set("view engine", "html");
 
 // sass middleware
 app.use(
-	'/page',
+	"/page",
 	sass({
-		src: path.join(__dirname, 'source'), // Input SASS source folder
-		dest: path.join(__dirname, '../page'), // Output CSS destination folder
+		src: path.join(__dirname, "source"), // Input SASS source folder
+		dest: path.join(__dirname, "../page"), // Output CSS destination folder
 		debug: false,
 		app,
-		base: '/page',
+		base: "/page",
 		cwd: __dirname,
-	}),
+	})
 );
 
 // rollup middleware
-app.use(
-	'/page',
-	rollup({
-		src: 'source',
-		dest: '../page',
-		cwd: __dirname,
-		debug: false,
-		//prefix: '/js',
-	}),
-);
+const rollupOpt = {
+	src: "source",
+	dest: "../page",
+	cwd: __dirname,
+	debug: false,
+	//prefix: '/js',
+	plugins: [],
+};
+if (!isDev()) rollupOpt.plugins.push(terser());
+
+app.use("/page", rollup(rollupOpt));
 
 // minify html middleware
-if (!isDev()) app.use('/page', minifyHtmlMiddleware({ debug: false }));
+if (!isDev()) app.use("/page", minifyHtmlMiddleware({ debug: false }));
 // engine ends
 
 // browser-sync start
@@ -101,15 +103,15 @@ if (isDev()) {
 		files: [
 			//__dirname,
 			{
-				match: ['views/**/*.njk', 'source/**/*.scss', 'source/**/*.js'],
+				match: ["views/**/*.njk", "source/**/*.scss", "source/**/*.js"],
 				fn: function (event, file) {
 					/** Custom event handler **/
-					console.log('[Browsersync]', event, file);
+					console.log("[Browsersync]", event, file);
 					browserSync.reload();
 				},
 			},
 		],
-		ignore: ['**/.git*', '**/tmp/**', '**/build/**'],
+		ignore: ["**/.git*", "**/tmp/**", "**/build/**"],
 		cors: true,
 	});
 	app.use(inject(bs));
@@ -118,41 +120,49 @@ if (isDev()) {
 
 // server static files
 // app.use(express.static(path.join(__dirname, '../page')));
-app.use('/page/assets', express.static(path.join(__dirname, '../page/assets')));
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use('/page/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use('/favicon.ico', async function (_, res) {
-	const read = fs.readFileSync(path.join(__dirname, 'source/assets/img/w-icon-25.png'));
-	res.setHeader('content-type', 'image/png');
+app.use("/page/assets", express.static(path.join(__dirname, "../page/assets")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+app.use(
+	"/page/node_modules",
+	express.static(path.join(__dirname, "node_modules"))
+);
+app.use("/favicon.ico", async function (_, res) {
+	const read = fs.readFileSync(
+		path.join(__dirname, "source/assets/img/w-icon-25.png")
+	);
+	res.setHeader("content-type", "image/png");
 	res.send(read);
 });
 // static files ends
 
-let identifier = 'GEN-HASH';
+let identifier = "GEN-HASH";
 
 // dynamic routes
-app.use('/page/:permalink', async function (req, res, next) {
-	if (identifier === 'GEN-HASH') {
+app.use("/page/:permalink", async function (req, res, next) {
+	if (identifier === "GEN-HASH") {
 		const github = new git(__dirname);
 		identifier = await github.latestCommit();
 	}
 
 	let { permalink } = req.params;
-	if (permalink.length === 0) permalink = 'index';
+	if (permalink.length === 0) permalink = "index";
 	let basename = path.basename(permalink, path.extname(permalink));
-	if (basename.length === 0) basename = 'index';
+	if (basename.length === 0) basename = "index";
 	const dirname = path.dirname(permalink);
-	const realpath = path.join(__dirname, 'views', dirname, basename + '.njk');
-	let pathname = new URL('http://' + req.hostname + req.url).pathname;
-	if (pathname.length === 0) pathname = 'index';
-	writefile('tmp/routes/' + pathname + '.log', JSON.stringify({ identifier, dirname, basename, realpath }, null, 2));
+	const realpath = path.join(__dirname, "views", dirname, basename + ".njk");
+	let pathname = new URL("http://" + req.hostname + req.url).pathname;
+	if (pathname.length === 0) pathname = "index";
+	writefile(
+		"tmp/routes/" + pathname + ".log",
+		JSON.stringify({ identifier, dirname, basename, realpath }, null, 2)
+	);
 	if (fs.existsSync(realpath)) {
 		res.render(realpath, { identifier }, function (err, html) {
 			if (err) {
-				console.log('fail render', permalink);
-				res.render('404');
+				console.log("fail render", permalink);
+				res.render("404");
 			} else {
-				console.log('success render', permalink);
+				console.log("success render", permalink);
 				res.send(html);
 			}
 		});
