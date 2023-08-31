fa416f75c487eb0342074689a5441143c07acd66
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-31 09:11:04 +0700

    update query

--- page-source/source/assets/js/google/anaylitcs-queries.js
+++ page-source/source/assets/js/google/anaylitcs-queries.js
@@ -7,21 +7,70 @@
  * @param {{ startDate?: string, endDate?: string, value: string, viewId: string, [key:string]: any }} value
  * @returns
  */
-const byPath = ({ startDate, endDate, value, viewId }) => {
+export const byPath = ({ startDate, endDate, value, viewId }) => {
   return {
-    viewId,
-    dimensions: [{ name: 'pagePath' }],
-    metrics: [{ name: 'screenPageViews' }],
-    dateRanges: [{ startDate: startDate || 'yesterday', endDate: endDate || 'today' }],
-    dimensionFilter: {
-      filter: {
-        fieldName: 'pagePath',
-        stringFilter: { matchType: 'EXACT', value },
+    reportRequests: [
+      {
+        dateRanges: [
+          {
+            startDate,
+            endDate,
+          },
+        ],
+        dimensions: [
+          {
+            name: 'ga:pagePath',
+          },
+        ],
+        viewId: viewId || 'ga:159996509',
+        metrics: [
+          {
+            expression: 'ga:pageviews',
+          },
+        ],
+        dimensionFilterClauses: [
+          {
+            filters: [
+              {
+                dimensionName: 'ga:pagePath',
+                operator: 'IN_LIST' /*EXACT*/,
+                expressions: [value],
+              },
+            ],
+          },
+        ],
       },
-    },
-    orderBys: [{ dimension: { orderType: 'ALPHANUMERIC', dimensionName: 'pagePath' } }],
-    metricAggregations: ['TOTAL'],
+    ],
   };
 };
 
-module.exports = { byPath };
+/**
+ * get all page views with the path
+ * @param {{ [key:string]: any; startDate: string, endDate: string; viewId: string; }} param0
+ * @returns
+ */
+export const getAllPagePathsWithView = ({ startDate, endDate, viewId }) => {
+  return {
+    reportRequests: [
+      {
+        dateRanges: [
+          {
+            startDate,
+            endDate,
+          },
+        ],
+        dimensions: [
+          {
+            name: 'ga:pagePath',
+          },
+        ],
+        viewId: viewId || 'ga:159996509',
+        metrics: [
+          {
+            expression: 'ga:pageviews',
+          },
+        ],
+      },
+    ],
+  };
+};
