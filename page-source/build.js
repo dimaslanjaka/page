e9d925b4e7e9a7f037fb1347a0fb626cb390453e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-31 10:52:32 +0700

    validate element exists before deletion

--- page-source/build.js
+++ page-source/build.js
@@ -13,7 +13,8 @@ async function buildPage() {
     const dom = new jsdom(response.data);
     const window = dom.window;
     // remove browsersync script
-    window.document.querySelector('script#__bs_script__').remove();
+    const bsScript = window.document.querySelector('script#__bs_script__');
+    if (bsScript) bsScript.remove();
     const html = dom.serialize();
     const minified = await terserHtml.minify(html, { collapseWhitespace: true });
     writefile(path.join(process.cwd(), route.dest), minified);
