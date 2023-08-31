b094ebc436f49dbec63d75d7448ff96e2dcf8d3b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-31 10:49:51 +0700

    fix EOL

--- page-source/build.js
+++ page-source/build.js
@@ -6,26 +6,26 @@ const { default: _git } = require('git-command-helper');
 const jsdom = require('jsdom').JSDOM;
 
 async function buildPage() {
-	const routes = config.routes;
-	for (let i = 0; i < routes.length; i++) {
-		const route = routes[i];
-		const response = await axios.get(route.src);
-		const dom = new jsdom(response.data);
-		const window = dom.window;
-		// remove browsersync script
-		window.document.querySelector('script#__bs_script__').remove();
-		const html = dom.serialize();
-		const minified = await terserHtml.minify(html, { collapseWhitespace: true });
-		writefile(path.join(process.cwd(), route.dest), minified);
-	}
-	// _gulp.series('page:commit')();
+  const routes = config.routes;
+  for (let i = 0; i < routes.length; i++) {
+    const route = routes[i];
+    const response = await axios.get(route.src);
+    const dom = new jsdom(response.data);
+    const window = dom.window;
+    // remove browsersync script
+    window.document.querySelector('script#__bs_script__').remove();
+    const html = dom.serialize();
+    const minified = await terserHtml.minify(html, { collapseWhitespace: true });
+    writefile(path.join(process.cwd(), route.dest), minified);
+  }
+  // _gulp.series('page:commit')();
 
-	// copy public-source into ../page
-	await fs.copy(path.join(__dirname, 'public-source'), path.join(__dirname, '../page'), { overwrite: true });
+  // copy public-source into ../page
+  await fs.copy(path.join(__dirname, 'public-source'), path.join(__dirname, '../page'), { overwrite: true });
 }
 
 if (require.main === module) {
-	buildPage();
+  buildPage();
 }
 
 module.exports = { buildPage };
