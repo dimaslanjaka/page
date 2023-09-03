f495fadbb0be7272917428f00209ce6f845305fa
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-03 21:03:19 +0700

    fix glob pattern by installing minimatch

--- gulpfile.js
+++ gulpfile.js
@@ -7,78 +7,78 @@ const pagepkg = require('./page/package.json');
 const { buildPage } = require('./page-source/build');
 
 gulp.task('page:copy', async function () {
-	// copy non-compiled files
-	// copy workflow
-	const w_src = path.join(__dirname, '.github');
-	const w_dest = path.join(__dirname, 'page/.github');
+  // copy non-compiled files
+  // copy workflow
+  const w_src = path.join(__dirname, '.github');
+  const w_dest = path.join(__dirname, 'page/.github');
 
-	if (fs.existsSync(w_dest)) fs.rmSync(w_dest, { force: true, recursive: true });
-	await fs.copy(w_src, w_dest, { overwrite: true });
+  if (fs.existsSync(w_dest)) fs.rmSync(w_dest, { force: true, recursive: true });
+  await fs.copy(w_src, w_dest, { overwrite: true });
 });
 
 const sleep = milliseconds => {
-	return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
 
 gulp.task('page:commit', async function () {
-	const currentGit = new git(__dirname);
-	const pageGit = new git(path.join(__dirname, 'page'));
-	const currentHash = await currentGit.latestCommit();
-	try {
-		await pageGit.add('-A');
-		const url = `${(await currentGit.getremote()).fetch.url.replace(/(.git|\/)$/, '')}/commit/${currentHash}`;
-		await pageGit.commit('update from ' + url);
-	} catch {
-		//
-	}
-	await sleep(700);
-	let canPush = await pageGit.canPush();
-	if (canPush) pageGit.push();
+  const currentGit = new git(__dirname);
+  const pageGit = new git(path.join(__dirname, 'page'));
+  const currentHash = await currentGit.latestCommit();
+  try {
+    await pageGit.add('-A');
+    const url = `${(await currentGit.getremote()).fetch.url.replace(/(.git|\/)$/, '')}/commit/${currentHash}`;
+    await pageGit.commit('update from ' + url);
+  } catch {
+    //
+  }
+  await sleep(700);
+  let canPush = await pageGit.canPush();
+  if (canPush) pageGit.push();
 });
 
 gulp.task('page:build', buildPage);
 
 gulp.task('page:clean', function () {
-	return new Promise(resolve => {
-		const base = path.join(__dirname, 'page');
-		const devPagePkg = Object.keys(pagepkg.devDependencies).map(str => '**/' + str + '/**');
-		const unusedFilesInNodeModules = Bluebird.all(
-			glob.glob(
-				[
-					'**/*.md',
-					'**/*.html',
-					'**/*.flow',
-					'**/bower.json',
-					'**/*.d.ts',
-					'**/.eslint*',
-					'**/eslintrc*',
-					'**/nodejs-package-types',
-					'**/git-command-helper',
-					'**/hexo-*',
-					'**/cross-spawn',
-					'**/@expo',
-					'**/binary-collections',
-					'**/deepmerge-ts',
-					'**/.package-lock.json',
-					'**/execa',
-					'**/sbg-*',
-					'**/@types',
-					'**/@eslint',
-					'**/@typescript',
-					'**/rmdir',
-				].concat(devPagePkg),
-				{
-					cwd: path.join(base, 'node_modules'),
-				},
-			),
-		)
-			.map(str => path.join(base, 'node_modules', str))
-			.each(str => fs.rmSync(str, { recursive: true, force: true }));
+  return new Promise(resolve => {
+    const base = path.join(__dirname, 'page');
+    const devPagePkg = Object.keys(pagepkg.devDependencies).map(str => '**/' + str + '/**');
+    const unusedFilesInNodeModules = Bluebird.all(
+      glob.glob(
+        [
+          '**/*.md',
+          '**/*.html',
+          '**/*.flow',
+          '**/bower.json',
+          '**/*.d.ts',
+          '**/.eslint*',
+          '**/eslintrc*',
+          '**/nodejs-package-types',
+          '**/git-command-helper',
+          '**/hexo-*',
+          '**/cross-spawn',
+          '**/@expo',
+          '**/binary-collections',
+          '**/deepmerge-ts',
+          '**/.package-lock.json',
+          '**/execa',
+          '**/sbg-*',
+          '**/@types',
+          '**/@eslint',
+          '**/@typescript',
+          '**/rmdir',
+        ].concat(devPagePkg),
+        {
+          cwd: path.join(base, 'node_modules'),
+        },
+      ),
+    )
+      .map(str => path.join(base, 'node_modules', str))
+      .each(str => fs.rmSync(str, { recursive: true, force: true }));
 
-		Bluebird.all(unusedFilesInNodeModules).finally(() => {
-			resolve(null);
-		});
-	});
+    Bluebird.all(unusedFilesInNodeModules).finally(() => {
+      resolve(null);
+    });
+  });
 });
 
 gulp.task('page', gulp.series('page:build', 'page:copy', 'page:clean', 'page:commit'));
