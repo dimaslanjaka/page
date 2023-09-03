729d534dceee99761b34f94cc1bb264204cb5d97
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-03 21:20:07 +0700

    feat: delete all cookies

--- page-source/source/assets/js/cookie.js
+++ page-source/source/assets/js/cookie.js
@@ -70,8 +70,19 @@ function removeCookie(name) {
   return eraseCookie(name);
 }
 
+function deleteAllCookies() {
+  var cookies = document.cookie.split(';');
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    var eqPos = cookie.indexOf('=');
+    var name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  }
+}
+
 /*if (typeof module === 'object' && 'exports' in module) {
   module.exports = { getCookie, removeCookie, eraseCookie, setCookie, createCookieMins };
 }*/
 
-module.exports = { getCookie, getCookies, removeCookie, eraseCookie, setCookie, createCookieMins };
+module.exports = { getCookie, getCookies, removeCookie, eraseCookie, setCookie, createCookieMins, deleteAllCookies };
