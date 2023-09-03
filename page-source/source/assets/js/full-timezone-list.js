be3a02de3503b8cfdb47d9ff79d84f28af2421ca
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-03 21:18:03 +0700

    fix: encoding

--- page-source/source/assets/js/full-timezone-list.js
+++ page-source/source/assets/js/full-timezone-list.js
@@ -4,36 +4,36 @@ const moment = require('moment-timezone');
 const table = document.getElementById('table-timezones');
 
 const theadInner = timezones
-	.flatMap(timezone => {
-		return Object.keys(timezone);
-	})
-	.filter(function (elem, index, self) {
-		return index === self.indexOf(elem);
-	})
-	.map(str => `<th>${str}</th>`)
-	.join('');
+  .flatMap(timezone => {
+    return Object.keys(timezone);
+  })
+  .filter(function (elem, index, self) {
+    return index === self.indexOf(elem);
+  })
+  .map(str => `<th>${str}</th>`)
+  .join('');
 
 table.querySelector('thead').innerHTML = `<tr>${theadInner}<th>clock</th><th>moment-timezone</th></tr>`;
 
 const tbodyInner = timezones
-	.map(timezone => {
-		return `<tr><td>${timezone.value}</td><td>${timezone.abbr}</td><td>${timezone.offset}</td><td>${
-			timezone.isdst
-		}</td><td>${timezone.text}</td><td>${timezone.utc
-			.map(zone => {
-				return `<li>${zone}</li>`;
-			})
-			.join('')}</td><td>${timezone.utc
-			.map(zone => {
-				return `<li><span timezone="${zone}">${moment().tz(zone).format()}</span></li>`;
-			})
-			.join('')}</td><td>${timezone.utc
-			.map(zone => {
-				return `<li><code code="${zone}">moment().tz("${zone}").format()</code></li>`;
-			})
-			.join('')}</td></tr>`;
-	})
-	.join('');
+  .map(timezone => {
+    return `<tr><td>${timezone.value}</td><td>${timezone.abbr}</td><td>${timezone.offset}</td><td>${
+      timezone.isdst
+    }</td><td>${timezone.text}</td><td>${timezone.utc
+      .map(zone => {
+        return `<li>${zone}</li>`;
+      })
+      .join('')}</td><td>${timezone.utc
+      .map(zone => {
+        return `<li><span timezone="${zone}">${moment().tz(zone).format()}</span></li>`;
+      })
+      .join('')}</td><td>${timezone.utc
+      .map(zone => {
+        return `<li><code code="${zone}">moment().tz("${zone}").format()</code></li>`;
+      })
+      .join('')}</td></tr>`;
+  })
+  .join('');
 
 table.querySelector('tbody').innerHTML = tbodyInner;
 
