6ed095c235d9b5ca6871870e8a0828eee52b5c46
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-04 20:38:29 +0700

    detect module.export

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,14 @@
-require('./analystic');
-require('bootstrap/dist/js/bootstrap.bundle.js');
-require('./r-ads');
+function loadMainScript() {
+  require('./analystic');
+  require('bootstrap/dist/js/bootstrap.bundle.js');
+  require('./r-ads');
+}
+
+if (typeof require != 'undefined' && typeof module != 'undefined' && require.main === module) {
+  // load script
+  loadMainScript();
+}
+
+if (typeof module === 'object' && 'exports' in module) {
+  module.exports = { loadMainScript };
+}
