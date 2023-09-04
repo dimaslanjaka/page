6ab62e1d289337a70fefbe96bec9e51003251a08
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-05 05:50:21 +0700

    add dev tool

--- webpack.config.js
+++ webpack.config.js
@@ -100,4 +100,7 @@ module.exports = {
     aggregateTimeout: 1000, // delay for compile
     ignored: /node_modules/,
   },
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: 'eval-source-map',
 };
