cdbf1f29bffce16dd1ff1f57d33ad6a01fc7e757
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-05 10:53:16 +0700

    implement chunk optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,66 @@
 const path = require('path');
 const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
+/**
+ * @type {import('webpack').Configuration}
+ */
 module.exports = {
-  // The entry point file described above
-  entry: './src/index.js',
-  // The location of the build folder described above
+  /**
+   * The entry point file
+   * [prevent duplication docs](https://webpack.js.org/guides/code-splitting/#prevent-duplication)
+   */
+  entry: {
+    // shared utility
+    shared: {
+      import: ['bluebird', 'crypto-js'],
+    },
+    // main script
+    index: {
+      import: './src/index.js',
+      dependOn: ['shared', 'firebase', 'internal'],
+    },
+    // all imported firebase module
+    firebase: {
+      import: ['firebase/app', 'firebase/auth'],
+      dependOn: 'shared',
+    },
+    // internal/local utility
+    internal: {
+      import: ['./src/utils/index.ts'],
+      dependOn: 'shared',
+    },
+  },
+  /**
+   * The location of the build folder
+   */
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+  },
+  /**
+   * Prevent duplication
+   * [docs](https://webpack.js.org/guides/code-splitting/#prevent-duplication)
+   */
+  optimization: {
+    // enables you to fine-tune how chunks are generated
+    runtimeChunk: 'multiple',
+    splitChunks: {
+      chunks: 'all',
+      // Set to 0 to enforce splitting regardless of size
+      minSize: 0,
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          type: 'css/mini-extract',
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
@@ -31,13 +84,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          // Creates `style` nodes from JS strings
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+        ],
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           'css-loader',
           // Compiles Sass to CSS
@@ -67,6 +125,19 @@ module.exports = {
   resolve: {
     extensions: ['.*', '.ts', '.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve('src', 'main.html'),
+      publicPath: '/',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
+    }),
+  ],
   /**
    * [Docs](https://webpack.js.org/configuration/dev-server/)
    * @type {import('webpack-dev-server').Configuration}
