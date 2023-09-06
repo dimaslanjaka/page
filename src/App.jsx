909cb1d0dbab3a8063af915298396f730a063060
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-06 17:57:46 +0700

    moved layout function to their own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Home } from './route/Home';
 import { Login } from './route/Login';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './assets/css/main.scss';
 import { loadMainScript } from './assets/js/main';
-import { Link } from './components/Link';
-import { Image } from './components/Image';
+import { Layout, NoMatch } from './components/Layout';
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,139 +30,6 @@ class App extends React.Component {
       </React.Fragment>
     );
   }
-}
-
-function Layout() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" id="navbar-top">
-        <div className="container-fluid">
-          <Link className="navbar-brand" href="/page">
-            <Image
-              src="//www.webmanajemen.com/favicon.ico"
-              alt="Logo"
-              width="30"
-              height="30"
-              className="d-inline-block align-text-top"
-            />
-            {/*WMI*/}
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" href="#">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="login" className="nav-link">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" href="#">
-                      Action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" href="#">
-                      Another action
-                    </Link>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" href="#">
-                      Something else here
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-
-      <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div className="col-md-4 d-flex align-items-center">
-            <Link href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-              <i className="fab fa-boostrap"></i>
-            </Link>
-            <span className="mb-3 mb-md-0 text-muted">© {new Date().getFullYear()} WMI, Inc</span>
-          </div>
-
-          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li className="ms-3">
-              <Link className="text-muted" href={`https://wa.me/+6285655667573?text=${encodeURI('Hi, Webmaster WMI')}`}>
-                <i className="fab fa-whatsapp"></i>
-              </Link>
-            </li>
-            <li className="ms-3">
-              <Link className="text-muted" href="https://github.com/dimaslanjaka">
-                <i className="fab fa-github"></i>
-              </Link>
-            </li>
-            <li className="ms-3">
-              <Link className="text-muted" href="https://fb.me/dimaslanjaka1">
-                <i className="fab fa-facebook"></i>
-              </Link>
-            </li>
-          </ul>
-        </footer>
-      </div>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/page">Go to the home page</Link>
-      </p>
-    </div>
-  );
 }
 
 export default App;
