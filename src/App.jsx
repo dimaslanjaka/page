698229089784b780fff6d40393beaddd84ea4d36
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-08 05:13:28 +0700

    import funcs from src/assets/js/main.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Home } from './route/Home';
 import { Login } from './route/Login';
 import { Route, Routes } from 'react-router-dom';
 import './assets/css/main.scss';
-import { loadMainScript } from './assets/js/main';
 import { Layout, NoMatch } from './components/Layout';
 import { Safelink } from './route/Safelink';
+import { all } from 'bluebird';
+import { copyTextToClipboard, loadJS, randomStr } from './utils';
+
 /*
 https://codesandbox.io/s/mzj1j0ryxx?file=/src/Icons.js
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -42,3 +44,44 @@ class App extends React.Component {
 }
 
 export default App;
+
+export async function loadMainScript() {
+  loadJS('/page/assets/js/analystic.js');
+  await import('bootstrap/dist/js/bootstrap.bundle.js');
+  await initClipBoard();
+  //loadJS('/page/assets/js/r-ads.js');
+}
+
+export function initClipBoard() {
+  return all(Array.from(document.querySelectorAll('pre'))).each(function (codeBlock) {
+    if (!codeBlock.getAttribute('id')) {
+      codeBlock.setAttribute('id', randomStr(4));
+    }
+
+    var button = document.createElement('button');
+    button.className = 'copy-code-button';
+    button.type = 'button';
+    var s = codeBlock.innerText;
+    button.setAttribute('data-clipboard-text', s);
+    button.innerText = 'Copy';
+    // button.setAttribute('title', 'Copiar para a área de transferência');
+    button.onclick = function (e) {
+      const el = document.getElementById(codeBlock.getAttribute('id'));
+      copyTextToClipboard(el.textContent.replace(/(Copy|Copied)$/gm, ''), e)
+        .then(() => {
+          e.target.textContent = 'Copied';
+        })
+        .finally(() => {
+          window.setTimeout(function () {
+            e.target.textContent = 'Copy';
+          }, 2000);
+        })
+        .catch(console.error);
+    };
+
+    // var pre = codeBlock.parentNode;
+    //codeBlock.classList.add('prettyprint');
+    // pre.parentNode.insertBefore(button, pre);
+    codeBlock.appendChild(button);
+  });
+}
