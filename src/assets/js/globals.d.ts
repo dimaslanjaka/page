8c0cd39db105aed2d6f4201b196fa2c9c0bd04c0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-11 08:48:29 +0700

    update types

--- src/assets/js/globals.d.ts
+++ src/assets/js/globals.d.ts
@@ -1,10 +1,14 @@
-interface Window {
-  dataLayer: Record<string, any>[];
-  opera: Record<string, any>;
-  opr: Record<string, any>;
-  safari: Record<string, any>;
-  adsense_option: AdsenseOption;
-  adsbygoogle: any;
+import { allAds } from '../../utils/adsense/config';
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[];
+    opera: Record<string, any>;
+    opr: Record<string, any>;
+    safari: Record<string, any>;
+    adsense_option: AdsenseOption;
+    adsbygoogle: any;
+  }
 }
 
 interface AdsenseOption {
@@ -12,4 +16,8 @@ interface AdsenseOption {
    * remove banner when parent width is 0 or display: none
    */
   remove?: boolean;
+  /**
+   * current ad slot
+   */
+  currentSlot?: (typeof allAds)[number];
 }
