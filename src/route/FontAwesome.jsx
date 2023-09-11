dd6841b684bd4f83240b6ff04e9d776bedbe0643
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-11 10:08:30 +0700

    clean src

--- src/route/FontAwesome.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-//import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
-
-export function FontAwesome() {
-  //return <>{icon({ name: 'user', family: 'sharp', style: 'solid' })}</>;
-  return <></>;
-}
