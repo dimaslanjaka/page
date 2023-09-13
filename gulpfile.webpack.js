150a28ed2d15b848a989b6e53e7c510670601263
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-13 07:01:18 +0700

    implement multiple webpack configs

--- gulpfile.webpack.js
+++ gulpfile.webpack.js
@@ -1,29 +1,74 @@
 const { spawn } = require('git-command-helper');
 const path = require('path');
 const webpack = require('webpack');
+const babelConfig = require('./.babelrc').config;
 
-const config = {
-  entry: './src/utils/adsense/index.ts',
-  output: {
-    filename: 'r-ads.js',
-    path: path.resolve(__dirname, 'dist/assets/js'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
-  },
-};
+/**
+ * create webpack config
+ * @param {string} entry
+ * @param {string} output
+ * @returns {webpack.Configuration}
+ */
+function createConfig(entry, output) {
+  /**
+   * @type {webpack.Configuration}
+   */
+  const override = {
+    entry,
+    output: {
+      filename: path.basename(output),
+      path: path.dirname(output),
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: 'ts-loader' },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: Object.assign(
+              {
+                cacheDirectory: './tmp/babel',
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+              },
+              babelConfig,
+            ),
+          },
+        },
+      ],
+    },
+    mode: 'production',
+  };
+  return override;
+}
 
 const buildStatic = done => {
-  webpack(config, (err, stats) => {
-    if (err || stats.hasErrors()) {
-      done(err);
-    }
-    // Done processing
-    done(null);
-  });
+  webpack(
+    [
+      createConfig('./src/utils/adsense/index.ts', path.join(__dirname, 'dist/assets/js/r-ads.js')),
+      createConfig('./public/page/assets/js/analystic.js', path.join(__dirname, 'dist/assets/js/analystic.js')),
+    ],
+    (err, stats) => {
+      if (stats) {
+        process.stdout.write(
+          stats.toString({
+            // Add console colors
+            colors: true,
+          }) + '\n',
+        );
+      }
+      if (err || stats.hasErrors()) {
+        done(err);
+      } else {
+        // Done processing
+        done(null);
+      }
+    },
+  );
 };
 
 const buildSite = done =>
