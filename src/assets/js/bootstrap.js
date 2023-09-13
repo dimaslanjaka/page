e8ee578f24122ce292ca2834e2aa6b4b64cc5b88
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-13 19:37:09 +0700

    load bootstrap module by await import

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -1,14 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import '@popperjs/core/dist/umd/popper.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+//import 'bootstrap/dist/css/bootstrap.css';
 
-document.querySelectorAll('.dropdown-toggle').forEach(item => {
-  item.addEventListener('click', event => {
-    console.log('click dropdown');
-    if (event.target.classList.contains('dropdown-toggle')) {
-      event.target.classList.toggle('toggle-change');
-    } else if (event.target.parentElement.classList.contains('dropdown-toggle')) {
-      event.target.parentElement.classList.toggle('toggle-change');
-    }
+export function initDropdown() {
+  document.querySelectorAll('.dropdown-toggle').forEach(item => {
+    item.addEventListener('click', event => {
+      console.log('click dropdown');
+      /** @type {HTMLElement} */
+      let el = event.target;
+      if (el.parentElement.classList.contains('dropdown-toggle')) {
+        el = el.parentElement;
+      }
+
+      el.classList.toggle('toggle-change');
+      if (el.classList.contains('toggle-change')) {
+        el.classList.toggle('show');
+        el.setAttribute('aria-expanded', 'true');
+      } else {
+        el.setAttribute('aria-expanded', 'false');
+      }
+
+      let menu = el.closest('.dropdown-menu');
+      if (!menu) {
+        menu = el.parentElement.querySelector('.dropdown-menu');
+      }
+      if (menu) {
+        menu.classList.toggle('show');
+      }
+    });
   });
-});
+}
+
+export async function loadBootstrapModule() {
+  await import('@popperjs/core/dist/umd/popper.min.js');
+  await import('bootstrap/dist/js/bootstrap.min.js');
+}
