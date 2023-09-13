eb35e8c965754174a2d1475077d3f16b7c8e955a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-14 02:50:18 +0700

    remove highlight.js loader cuz already loaded from main script

--- src/route/Highlight.jsx
+++ src/route/Highlight.jsx
@@ -1,13 +1,8 @@
-/* global hljs */
 import React from 'react';
-import './Highlight.scss';
 import { AdsenseFill } from '../components/Adsense';
+import './Highlight.scss';
 
 export function HighlightLayout() {
-  React.useEffect(() => {
-    initHljs();
-  });
-
   return (
     <main className="mx-auto p-2">
       <h1>Auto highlight.js</h1>
@@ -132,53 +127,4 @@ function initHljs() {
       </pre>
     </main>
   );
-}
-
-// document.addEventListener('DOMContentLoaded', initHljs);
-
-// start highlight pre code
-function startHighlighter(block) {
-  // validate hljs
-  if ('hljs' in window === false) return loadHljs();
-  // fix mysql highlight
-  if (block.classList.contains('language-mysql')) {
-    block.classList.remove('language-mysql');
-    block.classList.add('language-sql');
-  }
-  // start highlight pre code[data-highlight]
-  if (block.hasAttribute('data-highlight')) {
-    if (block.getAttribute('data-highlight') != 'false') {
-      // highlight on data-highlight="true"
-      hljs.highlightBlock(block);
-    }
-  } else {
-    // highlight no attribute data-highlight
-    hljs.highlightBlock(block);
-  }
-}
-
-function loadHljs() {
-  // validate hljs already imported
-  if ('hljs' in window === true) return;
-  // otherwise create one
-  const scr = document.createElement('script');
-  scr.src = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js';
-  scr.onload = initHljs;
-  const referenceNode = document.querySelectorAll('script').item(0);
-  referenceNode.parentNode.insertBefore(scr, referenceNode.nextSibling);
-}
-
-function initHljs() {
-  // highlight pre code
-  document.querySelectorAll('pre code').forEach(startHighlighter);
-
-  // highlight all pre code elements
-  // when use below syntax, please remove above syntax
-  /*
-  if ("initHighlightingOnLoad" in hljs) {
-    hljs.initHighlightingOnLoad();
-  } else if ("highlightAll" in hljs) {
-    hljs.highlightAll();
-  }
-  */
 }
