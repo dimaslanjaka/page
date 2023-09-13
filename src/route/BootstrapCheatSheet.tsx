3e5b950ae05d434c9a7314abb4743205f447a935
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-14 01:58:04 +0700

    add `react-bootstrap`

--- src/route/BootstrapCheatSheet.tsx
+++ src/route/BootstrapCheatSheet.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 export function BootstrapCheatSheet() {
   React.useEffect(() => {
@@ -122,6 +126,41 @@ export function BootstrapCheatSheet() {
           </div>
         </div>
       </div>
+      <h2>Off Canvas</h2>
+      <ExampleOffcanvas />
     </main>
   );
 }
+
+export interface ExampleOffcanvasProps {
+  className?: string | undefined;
+}
+
+const ExampleOffcanvas: React.FC<ExampleOffcanvasProps> = ({ className }) => {
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setShow(s => !s)} className={className}>
+        Toggle offcanvas
+      </Button>
+      <Offcanvas placement="end" show={show} onHide={() => setShow(false)}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title as="h5">Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div>
+            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists,
+            etc.
+          </div>
+
+          <DropdownButton variant="secondary" title="Dropdown button" className="mt-3">
+            <Dropdown.Item href="#">Action</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          </DropdownButton>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};
