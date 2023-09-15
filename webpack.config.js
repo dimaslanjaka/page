4bbfae0197eb52989bf21b0059d8c7a0bd3b91f7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-15 07:06:18 +0700

    add less loader

--- webpack.config.js
+++ webpack.config.js
@@ -264,6 +264,15 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
     ],
   },
   resolve: {
