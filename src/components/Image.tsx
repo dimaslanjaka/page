f64b15aeb5802d9031bb2ef68bb5d42d14c881c7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-16 10:32:47 +0700

    convert to typescript

---
+++ src/components/Image.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+interface ImageAttr {
+  src?: string;
+  fallbackSrc?: string;
+}
+
+interface ImageState extends ImageAttr {
+  errored: boolean;
+}
+
+/**
+ * image with fallback
+ */
+export class Image extends React.Component<ImageAttr, ImageState> {
+  defaultImage = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg';
+
+  constructor(props: Readonly<ImageAttr>) {
+    super(props);
+
+    this.state = {
+      src: props.src || this.defaultImage,
+      errored: false,
+    };
+  }
+
+  onError = () => {
+    if (!this.state.errored) {
+      this.setState({
+        src: this.props.fallbackSrc || this.defaultImage,
+        errored: true,
+      });
+    }
+  };
+
+  render() {
+    const { src } = this.state;
+    const { src: _1, fallbackSrc: _2, ...props } = this.props;
+
+    return <img src={src} onError={this.onError} {...props} />;
+  }
+}
