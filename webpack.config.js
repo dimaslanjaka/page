9986b3f8dc0c6c3dd734cb191de4473d1026edec
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-17 05:04:57 +0700

    update jsdoc usages

--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,9 @@ const stylesLoader = [
   {
     loader: 'css-loader',
     options: {
-      // Run `postcss-loader` on each CSS `@import` and CSS modules/ICSS imports, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
-      // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
-      importLoaders: 1,
+      // 1: Run `postcss-loader` on each CSS `@import` and CSS modules/ICSS imports, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+      // 2: If you need run `sass-loader` and `postcss-loader` on each CSS `@import`
+      importLoaders: 2,
       modules: {
         localIdentName: '[hash:base64]', // default
         auto: true, // default
