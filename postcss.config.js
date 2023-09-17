abc46619164ce3d8dc4e7735e6f10f5ca03b9d7e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-17 07:48:19 +0700

    using recommendation purgecss config source https://stackoverflow.com/a/67612097/6404439

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
-//const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const postcssPresetEnv = require('postcss-preset-env');
 
 /** @type {Parameters<typeof import('postcss')>[0]} */
 module.exports = {
   plugins: () => [
     postcssPresetEnv({ stage: 0 }),
-    // purgecss({
-    //   content: ['./**/*.html'],
-    // }),
+    purgecss({
+      content: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+      // Treat every word in the bundle as a CSS selector
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
   ],
 };
