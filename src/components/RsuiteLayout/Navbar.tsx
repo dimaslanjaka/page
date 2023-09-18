3a798ec492ca57dc50bf59bf72e37cb9c5afcf21
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-18 13:01:47 +0700

    fixed react lazy loading

--- src/components/RsuiteLayout/Navbar.tsx
+++ src/components/RsuiteLayout/Navbar.tsx
@@ -1,37 +1,55 @@
-import HomeIcon from '@rsuite/icons/legacy/Home';
 import React from 'react';
-import { Nav, Navbar } from 'rsuite';
+//
 
-// const Nav = React.lazy(() => import('rsuite/esm/Nav'));
+import HomeIcon from '@rsuite/icons/legacy/Home';
+// import { Nav, Navbar } from 'rsuite';
+
+const Nav = React.lazy(() => import('rsuite/esm/Nav'));
+const NavItem = React.lazy(() =>
+  import('rsuite/esm/Nav').then(module => ({
+    default: module.default.Item,
+  })),
+);
+const NavMenu = React.lazy(() =>
+  import('rsuite/esm/Nav').then(module => ({
+    default: module.default.Menu,
+  })),
+);
+const Navbar = React.lazy(() => import('rsuite/esm/Navbar'));
+const NavbarBrand = React.lazy(() =>
+  import('rsuite/esm/Navbar').then(module => ({
+    default: module.default.Brand,
+  })),
+);
+
 // const NavMenu = React.lazy(() => import('rsuite/esm/Nav/NavMenu'));
 // const NavItem = React.lazy(() => import('rsuite/esm/Nav/NavItem'));
-// const Navbar = React.lazy(() => import('rsuite/esm/Navbar'));
 // const NavbarBrand = React.lazy(() => import('rsuite/esm/Navbar/NavbarBrand'));
 // const HomeIcon = React.lazy(() => import('@rsuite/icons/legacy/Home'));
 
 const MyNavbar = ({ ...props }) => {
   return (
     <Navbar {...props}>
-      <Navbar.Brand href="#">WMI</Navbar.Brand>
+      <NavbarBrand href="#">WMI</NavbarBrand>
       <Nav>
-        <Nav.Item icon={<HomeIcon />} href="/page">
+        <NavItem icon={<HomeIcon />} href="/page">
           Home
-        </Nav.Item>
-        <Nav.Item href="/page/google/login">Login</Nav.Item>
-        <Nav.Menu title="Tools">
-          <Nav.Item onClick={navItemClick} href="/page/bot-detect">
+        </NavItem>
+        <NavItem href="/page/google/login">Login</NavItem>
+        <NavMenu title="Tools">
+          <NavItem onClick={navItemClick} href="/page/bot-detect">
             Selenium Checker
-          </Nav.Item>
-          <Nav.Item onClick={navItemClick} href="/page/moment-timezone">
+          </NavItem>
+          <NavItem onClick={navItemClick} href="/page/moment-timezone">
             Moment Timezone Playground
-          </Nav.Item>
-          <Nav.Item onClick={navItemClick} href="/page/cookies">
+          </NavItem>
+          <NavItem onClick={navItemClick} href="/page/cookies">
             Cookie Manager
-          </Nav.Item>
-        </Nav.Menu>
+          </NavItem>
+        </NavMenu>
       </Nav>
       {/* <Nav pullRight>
-        <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+        <NavItem icon={<CogIcon />}>Settings</NavItem>
       </Nav> */}
     </Navbar>
   );
