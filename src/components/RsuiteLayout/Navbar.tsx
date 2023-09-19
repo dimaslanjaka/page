0f90388a4da2ed50a3b21752a0f200dab7f05763
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-19 12:33:52 +0700

    navbar items as link

--- src/components/RsuiteLayout/Navbar.tsx
+++ src/components/RsuiteLayout/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 // import HomeIcon from '@rsuite/icons/legacy/Home';
 // import { Nav, Navbar } from 'rsuite';
-
+const Link = React.lazy(() => import('@components/Link'));
 const Nav = React.lazy(() => import('rsuite/esm/Nav'));
 const NavItem = React.lazy(() =>
   import('rsuite/esm/Nav').then(module => ({
@@ -29,18 +29,20 @@ const MyNavbar = ({ ...props }) => {
     <Navbar {...props}>
       <NavbarBrand href="#">WMI</NavbarBrand>
       <Nav>
-        <NavItem icon={<HomeIcon />} href="/page">
+        <NavItem icon={<HomeIcon />} as={Link} href="/page" eventKey="home">
           Home
         </NavItem>
-        <NavItem href="/page/google/login">Login</NavItem>
+        <NavItem href="/page/google/login" eventKey="login">
+          Login
+        </NavItem>
         <NavMenu title="Tools">
-          <NavItem onClick={navItemClick} href="/page/bot-detect">
+          <NavItem as={Link} href="/page/bot-detect">
             Selenium Checker
           </NavItem>
-          <NavItem onClick={navItemClick} href="/page/moment-timezone">
+          <NavItem as={Link} href="/page/moment-timezone">
             Moment Timezone Playground
           </NavItem>
-          <NavItem onClick={navItemClick} href="/page/cookies">
+          <NavItem as={Link} href="/page/cookies">
             Cookie Manager
           </NavItem>
         </NavMenu>
@@ -54,9 +56,9 @@ const MyNavbar = ({ ...props }) => {
 
 export default MyNavbar;
 
-function navItemClick(e: { target: any }): any {
-  const el = e.target as HTMLElement;
-  if (el.hasAttribute('href')) {
-    window.location.href = el.getAttribute('href');
-  }
-}
+// function navItemClick(e: { target: any }): any {
+//   const el = e.target as HTMLElement;
+//   if (el.hasAttribute('href')) {
+//     window.location.href = el.getAttribute('href');
+//   }
+// }
