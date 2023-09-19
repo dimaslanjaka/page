a0f5fd6e32a62f68391d129d89a080ca315f6387
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-19 07:56:38 +0700

    cleanup codes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-  useRoutes,
-} from 'react-router-dom';
+import { BrowserRouter, useRoutes } from 'react-router-dom';
 import { suspenseIt } from './utils/suspense';
 
 const RSuiteLayout = React.lazy(() => import('@components/RsuiteLayout'));
@@ -19,30 +12,6 @@ const Home = React.lazy(() => import('@route/HomePage'));
 const BotDetect = React.lazy(() => import('@route/BotDetect'));
 const HighlightLayout = React.lazy(() => import('@route/Highlight'));
 const MomentTimezone = React.lazy(() => import('@/route/MomentTimezone'));
-
-const _Router_jsx = () => {
-  const routes = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/page" element={suspenseIt(<RSuiteLayout />)}>
-        <Route index element={suspenseIt(<Home />)} />
-        <Route path="google/login" element={suspenseIt(<Login />)} />
-        <Route path="google/login.html" element={suspenseIt(<Login />)} />
-        <Route path="safelink" element={suspenseIt(<Safelink />)} />
-        <Route path="safelink.html" element={suspenseIt(<Safelink />)} />
-        <Route path="bot-detect" element={suspenseIt(<BotDetect />)} />
-        <Route path="bot-detect.html" element={suspenseIt(<BotDetect />)} />
-        <Route path="highlight-js" element={suspenseIt(<HighlightLayout />)} />
-        <Route path="highlight-js.html" element={suspenseIt(<HighlightLayout />)} />
-        <Route path="moment-timezone" element={suspenseIt(<MomentTimezone />)} />
-        <Route path="moment-timezone.html" element={suspenseIt(<MomentTimezone />)} />
-        <Route path="ui" element={<UI />} />
-        <Route path="*" element={<NoMatch />} />
-      </Route>,
-    ),
-  );
-
-  return <RouterProvider router={routes} />;
-};
 
 let _Router_array = () => {
   /**
