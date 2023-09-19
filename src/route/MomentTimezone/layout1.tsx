6a2131c32be109c48be3417d836cbbc420b42300
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-19 07:21:58 +0700

    separate layout

---
+++ src/route/MomentTimezone/layout1.tsx
@@ -0,0 +1,43 @@
+import { loadCSS } from '@/utils';
+import React from 'react';
+import Link from '../../components/Link';
+import ClockLive from './ClockLive';
+import DateInput from './DateInput';
+import FullTimezoneList from './FullTimezoneList';
+import { momentTimezonePlayground } from './moment-timezone';
+import { Snippet } from './Snippet';
+
+function MTLayout1() {
+  React.useEffect(() => {
+    document.title = 'Moment Timezone Playground';
+    momentTimezonePlayground();
+    loadCSS('//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+    require('./MomentTimezone.scss');
+  });
+
+  return (
+    <div>
+      <section>
+        <div className="text-center">
+          <h1>
+            <Link
+              href="https://www.webmanajemen.com/page/moment-timezone.html"
+              target="_blank"
+              className="text-decoration-none"
+            >
+              Moment Timezone Playground
+            </Link>
+          </h1>
+          <small className="text-muted">Parse and display dates in any timezone.</small>
+        </div>
+      </section>
+
+      <DateInput />
+      <Snippet />
+      <ClockLive />
+      <FullTimezoneList />
+    </div>
+  );
+}
+
+export default MTLayout1;
