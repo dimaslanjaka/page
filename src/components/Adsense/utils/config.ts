95214bc7170a0ebf0563dda39e9e5a24a09cfdd3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-23 06:58:56 +0700

    update import paths

--- src/components/Adsense/utils/config.ts
+++ src/components/Adsense/utils/config.ts
@@ -1,4 +1,4 @@
-import { array_shuffle } from '../../../utils';
+import { array_shuffle } from '@/utils';
 
 // initialize undefined window properties
 if (typeof window !== 'undefined' && typeof document !== 'undefined') {
