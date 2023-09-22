a100c118ab255f6e02862c4a75db7442609099ea
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-23 06:19:42 +0700

    update layout and styles

--- src/route/Highlight/index.jsx
+++ src/route/Highlight/index.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 
 const AdsenseFill = React.lazy(() => import('@components/Adsense/AdsenseFill'));
 const HighlightElement = React.lazy(() => import('@components/Highlight.js'));
+const Link = React.lazy(() => import('@components/Link'));
 
 function HighlightLayout() {
   React.useEffect(() => {
@@ -13,68 +14,73 @@ function HighlightLayout() {
   });
 
   return (
-    <div className="mx-auto p-2">
-      <h1>Auto highlight.js</h1>
-      <p>
-        implement auto syntax highlighting using <kbd>highlight.js</kbd>
-      </p>
-      <h4>Enable Highlighting</h4>
-      <HighlightElement lang="js">
-        {`
-function Shape2D(){
-  this.name = 'Shape 2D';
-}
-`}
-      </HighlightElement>
-      <h4>Disable Highlighting</h4>
-      <HighlightElement className="ss" data-highlight="false">
-        {`
-function Triangle(base,height){
-    this.name = 'Triangle';
-    this.base = base;
-    this.height = height;
-    this.getArea = function(){return this.base*this.height/2;};
-}
-`}
-      </HighlightElement>
-
-      <AdsenseFill />
-
-      <h4>
-        without <code>data-highlight</code> attribute
-      </h4>
-
-      <pre>
-        <code className="language-sql">
-          CREATE USER 'dimaslanjaka'@'%' IDENTIFIED VIA mysql_native_password USING '***';GRANT ALL PRIVILEGES ON *.* TO
-          'dimaslanjaka'@'%' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0
-          MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `dimaslanjaka\_%`.* TO
-          'dimaslanjaka'@'%';
-        </code>
-      </pre>
-
-      <pre>
-        <code>
+    <React.Suspense fallback={<div>Page loading</div>}>
+      <div className="text-center">
+        <h1>Auto highlight.js</h1>
+        <p>
+          implement auto syntax highlighting using <Link href="https://npmjs.com/highlight.js">highlight.js</Link>
+        </p>
+      </div>
+      <div className="mx-auto p-2">
+        <h4>Enable Highlighting</h4>
+        <HighlightElement lang="js">
           {`
+function Shape2D(){
+this.name = 'Shape 2D';
+}
+`}
+        </HighlightElement>
+        <h4>Disable Highlighting</h4>
+        <HighlightElement className="ss" data-highlight="false">
+          {`
+function Triangle(base,height){
+  this.name = 'Triangle';
+  this.base = base;
+  this.height = height;
+  this.getArea = function(){return this.base*this.height/2;};
+}
+`}
+        </HighlightElement>
+
+        <AdsenseFill />
+
+        <h4>
+          without <code>data-highlight</code> attribute
+        </h4>
+
+        <pre>
+          <code className="language-sql">
+            {`
+CREATE USER 'dimaslanjaka'@'%' IDENTIFIED VIA mysql_native_password USING '***';
+GRANT ALL PRIVILEGES ON *.* TO 'dimaslanjaka'@'%' REQUIRE NONE WITH GRANT OPTION
+MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+GRANT ALL PRIVILEGES ON \`dimaslanjaka\\_%\`.* TO 'dimaslanjaka'@'%';
+            `}
+          </code>
+        </pre>
+
+        <pre>
+          <code>
+            {`
 function Shape(){
   // this is a comment
   this.name = 'Shape';
   this.toString = function(){return this.name;};
 }
 `}
-        </code>
-      </pre>
+          </code>
+        </pre>
 
-      <h2>Full codes</h2>
-      <h3>CSS</h3>
-      <pre>
-        <code className="language-scss">{`
+        <h2>Full codes</h2>
+        <h3>CSS</h3>
+        <pre>
+          <code className="language-scss">{`
 @import url('//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.16.2/build/styles/default.min.css');
 `}</code>
-      </pre>
-      <h3>JS</h3>
-      <pre>
-        <code>{`
+        </pre>
+        <h3>JS</h3>
+        <pre>
+          <code>{`
 // init highlight.js after page loaded
 document.addEventListener('DOMContentLoaded', initHljs);
 
@@ -108,9 +114,9 @@ function loadHljs() {
   scr.onload = initHljs;
   const referenceNode = document.querySelectorAll('script').item(0);
   referenceNode.parentNode.insertBefore(scr, referenceNode.nextSibling);
-}
+  }
 
-function initHljs() {
+  function initHljs() {
   // highlight pre code
   document.querySelectorAll('pre code').forEach(startHighlighter);
 
@@ -124,17 +130,18 @@ function initHljs() {
   }
   */
 }
-      `}</code>
-      </pre>
-      <h3>HTML</h3>
-      <pre>
-        <code>{`
+    `}</code>
+        </pre>
+        <h3>HTML</h3>
+        <pre>
+          <code>{`
 <pre><code class="language-javascript">
 // your codes to print here
 </code></pre>
-      `}</code>
-      </pre>
-    </div>
+    `}</code>
+        </pre>
+      </div>
+    </React.Suspense>
   );
 }
 
