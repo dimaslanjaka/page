0cfd51090b9c254b14a5fb1cc86aebb68f79d21d
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-23 08:12:30 +0700

    add entries

--- gulpfile.webpack.js
+++ gulpfile.webpack.js
@@ -17,7 +17,7 @@ function createConfig(entry, output) {
     entry,
     output: {
       filename: path.basename(output),
-      path: path.dirname(output),
+      path: path.dirname(output)
     },
     resolve: {
       extensions: ['.ts', '.js', '.json'],
@@ -27,8 +27,8 @@ function createConfig(entry, output) {
         os: require.resolve('os-browserify'),
         buffer: require.resolve('buffer/'),
         constants: require.resolve('constants-browserify'),
-        stream: require.resolve('stream-browserify'),
-      },
+        stream: require.resolve('stream-browserify')
+      }
     },
     module: {
       rules: [
@@ -41,15 +41,15 @@ function createConfig(entry, output) {
             options: Object.assign(
               {
                 cacheDirectory: './tmp/babel',
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: ['@babel/preset-env', '@babel/preset-react']
               },
-              babelConfig,
-            ),
-          },
-        },
-      ],
+              babelConfig
+            )
+          }
+        }
+      ]
     },
-    mode: 'production',
+    mode: 'production'
   };
   return override;
 }
@@ -63,14 +63,16 @@ const buildStatic = done => {
     [
       createConfig('./src/components/Adsense/utils/index.ts', path.join(__dirname, 'dist/assets/js/r-ads.js')),
       createConfig('./public/page/assets/js/analystic.js', path.join(__dirname, 'dist/assets/js/analystic.js')),
+      createConfig('./src/utils/scroll-helper.ts', path.join(__dirname, 'dist/assets/js/remember-scroll-position.js')),
+      createConfig('./src/utils/scroll-helper.ts', path.join(__dirname, 'dist/assets/js/scroll-to-hash.js'))
     ],
     (err, stats) => {
       if (stats) {
         process.stdout.write(
           stats.toString({
             // Add console colors
-            colors: true,
-          }) + '\n',
+            colors: true
+          }) + '\n'
         );
       }
       if (err || stats.hasErrors()) {
@@ -79,7 +81,7 @@ const buildStatic = done => {
         // Done processing
         done(null);
       }
-    },
+    }
   );
 };
 
