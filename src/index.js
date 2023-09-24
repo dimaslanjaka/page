ddf9748f8f8330b3541521523c3efedcbf2e88d7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-25 05:24:51 +0700

    replace global promise with `bluebird`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+var Promise = require('bluebird');
+if (/dev/i.test(process.env.NODE_ENV)) {
+  // Configure webpack and browserify for development/debugging
+  Promise.config({
+    longStackTraces: true,
+    warnings: true, // note, run node with --trace-warnings to see full stack traces for warnings
+    // Enable cancellation
+    cancellation: true
+  });
+} else {
+  // Configure webpack and browserify for production/performance
+  Promise.config({
+    longStackTraces: false,
+    warnings: false,
+    // Enable cancellation
+    cancellation: true
+  });
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, useRoutes } from 'react-router-dom';
