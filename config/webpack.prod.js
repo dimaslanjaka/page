152113f4690ed4806f2feb784fc0096da93e3fff
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-29 22:09:14 +0700

    install webpack analyzer plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const paths = require('./paths');
 const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 /**
  * @type {import('webpack').Configuration}
  */
@@ -11,5 +13,18 @@ module.exports = merge(common, {
     path: paths.build,
     publicPath: paths.base,
     filename: 'runtime/js/[name].[contenthash].bundle.js'
-  }
+  },
+  plugins: [
+    // analyze webpack bundle
+    new BundleAnalyzerPlugin({
+      // report filename
+      reportFilename: 'runtime/index.html',
+      // generate report file in output directory
+      generateStatsFile: true,
+      // auto open browser
+      openAnalyzer: false,
+      // 'static': generate static html on production
+      analyzerMode: 'static'
+    })
+  ]
 });
