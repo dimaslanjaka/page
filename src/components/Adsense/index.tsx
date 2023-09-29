e1d2c0aa11a0af91f50b90396d69a4cfe4afe6c5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-29 17:45:18 +0700

    remove all ads property

--- src/components/Adsense/index.tsx
+++ src/components/Adsense/index.tsx
@@ -23,40 +23,6 @@ interface AdsenseState extends AdsenseProperties {
 }
 
 class Adsense extends React.Component<AdsenseProperties, AdsenseState> {
-  property = {};
-  allAds = [
-    {
-      name: 'kiki',
-      pub: '2188063137129806',
-      ads: [
-        {
-          style: 'display: block !important; text-align: center',
-          'data-ad-layout': 'in-article',
-          'data-ad-format': 'fluid',
-          'data-ad-slot': '5634823028'
-        },
-        {
-          style: 'display: block !important; text-align: center',
-          'data-ad-layout': 'in-article',
-          'data-ad-format': 'fluid',
-          'data-ad-slot': '8481296455'
-        },
-        {
-          style: 'display:block !important',
-          'data-ad-slot': '2667720583',
-          'data-ad-format': 'auto',
-          'data-full-width-responsive': 'true'
-        },
-        {
-          style: 'display:block !important',
-          'data-ad-format': 'fluid',
-          'data-ad-layout-key': '-gw-3+1f-3d+2z',
-          'data-ad-slot': '6979059162'
-        }
-      ]
-    }
-  ];
-
   constructor(props: AdsenseProperties) {
     super(props);
     const defaults: AdsenseState = {
