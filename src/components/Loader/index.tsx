4e0b62a343dd29621ba0e7856e5e36b51eac728c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-30 10:55:58 +0700

    export Spinner

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Loader as RLoader } from 'rsuite';
+import './Loader.scss';
 
 class Loader extends React.Component {
   componentDidMount() {
-    require('./Loader.scss');
+    // require('./Loader.scss');
     // waitUntilPageFullyLoaded(() => removeElement(querySelector('.loader')));
   }
 
   render() {
-    return <RLoader center content={<div className="sp sp-circle"></div>} className="loader" size="lg"></RLoader>;
+    return <RLoader center content={<Spinner />} className="loader" size="lg"></RLoader>;
   }
+}
+
+export function Spinner() {
+  return <div className="sp sp-circle"></div>;
 }
 
 export default Loader;
